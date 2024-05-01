b431dee870b2bb6b2fc701340487b70499f9bdd2
nucabebia16 <nucabebia4@hmail.com>
2024-05-01 20:26:21 +0200

    second commit

---
+++ day1/lesson1.py
@@ -0,0 +1,18 @@
+name="chemi saxelia nino"
+surname="bebia"
+age="var 23 wlis"
+height="chemi simaglea 165.5 santimetri"
+profession ="profesiit var  qartuli cekvis qoreografi"
+living_place="vcxovrob germaniashi"
+
+
+
+
+
+
+print(name + " " + surname + " " + str(age) + " " + str(height) + " " + profession + " " + living_place)
+
+knows_programming=True
+nino_lives_german= True
+
+print(nino_lives_german)
