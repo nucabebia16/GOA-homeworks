c365762e9a7c59f6ad01d5b2da24743d3f2c854b
nucabebia16 <nucabebia4@hmail.com>
2024-05-28 14:12:37 +0200

    seventh commit

---
+++ day7/homework2.py
@@ -0,0 +1,4 @@
+#შექმენი თქვენი რაზმის წევრების სია
+#დაპრინტეთ შუა 5 რაზმის წევრი(slicing)
+my_squad= [" gobejishvili" , " gorozia", " buliskeria", "kekelidze", " kereselidze", " qebulia"]
+print(my_squad[1:6])
