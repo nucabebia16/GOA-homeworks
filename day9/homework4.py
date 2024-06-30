48b6732c244e1ff40ecb7bd440f32ad6a6bb516c
nucabebia16 <nucabebia4@hmail.com>
2024-06-30 20:07:39 +0200

    ninth commit

---
+++ day9/homework4.py
@@ -0,0 +1,3 @@
+#დაპრინტე hello world  ოღონდ არა ასე print(hellow world)
+composers="antonio lucio vivaldi, ludwig van beerhoven, sergei rachmaninov "
+print(composers[38] , composers[35] , composers[8], composers[18], composers[3] ,composers[26] +  "  " +  composers[26] ,composers[39], composers[37] ,composers[8] ,composers[19])
--- homework5.py
+++
@@ -1,3 +0,0 @@
-#დაპრინტე  hellow world ოღონდ არა ასეprint(helllow world)
-composers="antonio lucio vivaldi, ludwig van beerhoven, sergei rachmaninoff"
-print(composers[38],composers[35],composers[8],composers[18],composers[3],composers[26]      +      composers[26],composers[39],composers[37],composers[8],composers[19])
